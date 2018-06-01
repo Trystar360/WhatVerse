79b49b2b0dae1244b2f6e87988ea8a5d2d80066d
Trystar360 <tristanfrederick360@gmail.com>
2018-06-01 08:52:26 -0400

    update

--- js/script.js
+++ js/script.js
@@ -1,38 +1,61 @@
 var bible;
-var output = '';
+var verse;
+
 
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
        bible = JSON.parse(xhttp.responseText)
-       console.log(length(bible));
        console.log(bible);
-       choseVerse();
+       console.log(bible.length);
+       //console.log(bible[0].chapters[0][0]);
        
     }
 };
-xhttp.open("GET", "https://raw.githubusercontent.com/honza/bibles/master/NIV/NIV.json", true);
+xhttp.open("GET", "../bible.json", true);
 xhttp.send();
 
 
 function choseVerse(){
+    verse = '';
+
+    var ref;
     var length, maxLength; 
     var book, books;
     var chapter,chapters;
     var verseStart;
     var verseEnd, verss;
 
-    maxLength = 10;
+    maxLength = 5;
     length = newRand(maxLength);
-    books = length(bible);
+    books = bible.length;
     book = newRand(books);
-    chapters = length(book);
-    chapter = newRand(book);
-    verss = newRand(book.length);
+    chapters = bible[book].chapters.length;
+    chapter = newRand(chapters);
+    verss = bible[book].chapters[chapter].length;
     verseStart = newRand(verss - length);
     verseEnd = verseStart + length;
 
-    console.log("length: " + length + ", book: " + book + ", chapter: " + chapter + ", passage: " + verseStart + "-" + verseEnd);
+    ref = bible[book].name + " " + chapter + ":" + verseStart + "-" + verseEnd;
+
+    for(i = verseStart; i < verseEnd; i ++){
+        verse += '<sup>' + i + '</sup>';
+        verse += '<p class="verse">' + bible[book].chapters[chapter][i] + '</p>'
+    }
+    console.log(verse);
+    document.getElementById("ref").innerHTML = ref;
+    document.getElementById("verses").innerHTML = verse;
+    // console.log("length: ")
+    // console.log(length);
+    // console.log("book: ");
+    // console.log(book);
+    // console.log("chapter: ");
+    // console.log(chapter);
+    // console.log("passage: ");
+    // console.log(verseStart);
+    // console.log("-" );
+    // console.log(verseEnd);
+
 }
 
 function newRand(n){
@@ -41,4 +64,17 @@ function newRand(n){
 
 function length(obj) {
     return Object.keys(obj).length;
+}
+
+function verseBtnPress(){
+    choseVerse();
+    document.getElementById("verseCard").style.display = "block";
+}
+
+function copy(s){
+    document.getElementById(s).execCommand("copy");
+}
+
+function copyClick(){
+    copy("verses");
 }
