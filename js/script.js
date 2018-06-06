47cab63b253743536a3eada62943d3d7f8790a66
Trystar360 <tristanfrederick360@gmail.com>
2018-06-05 23:21:24 -0400

    esv

--- js/script.js
+++ js/script.js
@@ -1,13 +1,30 @@
-var bible;
-var verse;
 
-var version = 0;
+//Global variables
+var bible = '';
+var verse;
+var xhttp;
+var global = this;
+var version = 1;
+var test;
+var first = true;
+
 //0 = KJV; 1 = ESV; 
 
 
+notif();
+function notif(){
+    M.toast({html: 'ESV is now the default version. I am working on letting you chose the version'},null);
+}
 
+function notif2(){
+    M.toast({html: 'For the time being you will have to press the button once more, working on a fix'});
+    M.toast({html: 'Sorry for the inconvenience.'});
+}
 
-
+/*
+Retrieves and parses json data
+@parameter n: path to json file
+*/
 function choseVerse(n){
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
@@ -21,53 +38,99 @@ function choseVerse(n){
     xhttp.send();  
 }
 
+/*
+@return: bible object
+*/
+function getBib(){
+    return bible;
+}
+
+/*
+Used to easily get random number.
+@parameter n: max number.
+@return: random number from 1 to n.
+*/
 function newRand(n){
     return Math.floor(Math.random() * n) + 1;
 }
 
+/*
+Used to easily get the length of JSON arrays
+@parameter obj: the array object to get the length of.
+@return: lengh of keys of obj.
+*/
 function length(obj) {
     return Object.keys(obj).length;
 }
 
+/*
+To be run on verse button press.
+*/
 function verseBtnPress(){
+    if(first == true){
+        notif2();
+        this.first = false;
+    }
+    
     switch(version){
-        case 0: choseVerse("bible.json"), kjv();
-        case 1: choseVerse("https://raw.githubusercontent.com/honza/bibles/master/ESV/ESV.json");
+
+        case 0:
+            choseVerse("../bible.json");
+            kjv();
+            break;
+
+        case 1: 
+        choseVerse("https://raw.githubusercontent.com/honza/bibles/master/ESV/ESV.json");
+        esv();
+        break;
     }
     
     
 }
 
+/*
+Used to test verse alg. runs verse x1000
+*/
 function testVerse(){
     for(var i = 0; i < 1000; i ++){
         choseVerse();
     }
 }
 
+/*
+Randomly choses verse from KJV json
+*/
 function kjv(){
     var ref;
     var length, maxLength; 
     var book, books;
-    var chapter,chapters;
+    var chapter,chapters_;
     var verseStart;
     var verseEnd, verss;
     verse = '';
+    var bible = this.bible;
 
-    document.getElementById("ref").innerHTML = ref;
-    document.getElementById("verses").innerHTML = verse;
-
+    
     maxLength = 5;
     length = newRand(maxLength);
     books = bible.length;
     book = newRand(books) - 1;
-    chapters = bible[book].chapters.length;
-    chapter = newRand(chapters) - 1;
+    console.log(bible);
+    console.log(book);
+    console.log(bible[book]);
+    console.log(bible[book].chapters)
+    chapters_ = bible[book].chapters.length;//yeet
+    chapter = newRand(chapters_) - 1;
     verss = bible[book].chapters[chapter].length;
     verseStart = newRand(verss - length);
     verseEnd = verseStart + length;
+
+    
      
     var tmp = chapter + 1;
     var tmpv = verseStart + 1;
+
+    console.log(bible[book].chapters[chapter][verseStart]);
 
     if(length > 1){
         ref = bible[book].name + " " + tmp + ":" + tmpv +"-" + verseEnd;
@@ -79,12 +142,68 @@ function kjv(){
     }else{
         ref = bible[book].name + " " + tmp + ":" + tmpv;
         verse += '<sup>' + tmp + '</sup>';
-            verse += '<p class="verse">' + bible[book].chapters[chapter][verseStart] + '</p>'
-            
+        verse += '<p class="verse">' + bible[book].chapters[chapter][verseStart] + '</p>'
+    }
+            document.getElementById("ref").innerHTML = ref;
+            document.getElementById("verses").innerHTML = verse;
             document.getElementById("verseCard").style.display = "block";
         
 
 }
+
+function cl(logged){
+    console.log(logged);
+}
+
+function esv(){
+    var ref;
+    var length, maxLength; 
+    var book, books, bookName;
+    var chapter,chapters_;
+    var verseStart;
+    var verseEnd, verss;
+    verse = '';
+    var bible = this.bible;
+
+    
+    maxLength = 5;
+    length = newRand(maxLength);
+    books = Object.keys(bible).length;
+    book = newRand(books) - 1;
+    bookName = Object.keys(bible)[book];
+    chapters_ = Object.keys(bible[bookName]).length;
+    chapter = newRand(chapters_);
+    console.log(bookName);
+    console.log(bible[bookName]);
+    console.log(chapter);
+    verss = Object.keys(bible[bookName][chapter]).length;;
+    verseStart = newRand(verss - length);
+    verseEnd = verseStart + length;
+
+    
+     
+    var tmp = chapter ;
+    var tmpv = verseStart ;
+
+    console.log(bible[bookName][chapter][verseStart]);
+
+    if(length > 1){
+        ref = bookName + " " + tmp + ":" + tmpv +"-" + verseEnd;
+        for(i = verseStart; i < verseEnd; i ++){
+            var tmpp = i + 1;
+            verse += '<sup>' + tmpp + '</sup>';
+            verse += '<p class="verse">' + bible[bookName][chapter][i] + '</p>'
+        } 
+    }else{
+        ref = bookName + " " + tmp + ":" + tmpv;
+        verse += '<sup>' + tmp + '</sup>';
+        verse += '<p class="verse">' + bible[bookName][chapter][verseStart] + '</p>'
+    }
+            document.getElementById("ref").innerHTML = ref;
+            document.getElementById("verses").innerHTML = verse;
+            document.getElementById("verseCard").style.display = "block";
+        
+
 }
 
 //test
